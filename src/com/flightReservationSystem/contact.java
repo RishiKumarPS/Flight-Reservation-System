696d52599edcd9875177507776bd667c2c9ec060
Rishi Kumar P S <22448119+RishiKumarPS@users.noreply.github.com>
2022-02-01 21:48:46 +0530

    initial

--- src/com/flightReservationSystem/contact.java
+++ src/com/flightReservationSystem/contact.java
@@ -3,6 +3,13 @@ package com.flightReservationSystem;
 class contact {
     private double phone;
     private String email;
+    private address address;
+
+    public contact(double phone, String email, com.flightReservationSystem.address address) {
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+    }
 
     public double getPhone() {
         return phone;
@@ -20,8 +27,11 @@ class contact {
         this.email = email;
     }
 
-    public contact(double phone, String email) {
-        this.phone = phone;
-        this.email = email;
+    public com.flightReservationSystem.address getAddress() {
+        return address;
+    }
+
+    public void setAddress(com.flightReservationSystem.address address) {
+        this.address = address;
     }
 }
